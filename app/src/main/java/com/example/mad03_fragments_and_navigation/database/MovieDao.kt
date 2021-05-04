e5c46fc25af19d41135ea3493c8b7934e915f056
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-05-04 17:35:19 +0200

    implemented entity and dao

--- app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
@@ -1,5 +1,24 @@
 package com.example.mad03_fragments_and_navigation.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.mad03_fragments_and_navigation.models.Movie
+
+@Dao
 interface MovieDao {
-    // TODO implement me
+
+    @Insert
+    fun createMovie(movie: Movie): Long
+
+    @Update
+    fun update(movie: Movie)
+
+    @Delete
+    fun delete(movieId: Long)
+
+    @Query("DELETE FROM movie_table")
+    fun clear()
+
+    @Query("SELECT * FROM movie_table ORDER BY id DESC")
+    fun getAll(): LiveData<List<Movie>>
 }
